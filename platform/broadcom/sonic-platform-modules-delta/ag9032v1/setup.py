811b5392c98c9bb8d16783a6d21dcd3747428e75
Andreas Florath <andreas@florath.net>
2022-08-17 10:51:53 +0000

    Fixed debian and setup files for sonic_platform; added led_status.sh script to debian package
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- platform/broadcom/sonic-platform-modules-delta/ag9032v1/setup.py
+++ platform/broadcom/sonic-platform-modules-delta/ag9032v1/setup.py
@@ -11,13 +11,13 @@ setup(
     author='SONiC Team',
     author_email='linuxnetdev@microsoft.com',
     url='https://github.com/Azure/sonic-buildimage',
-    maintainer='Unkown',
+    maintainer='unkown',
     maintainer_email='unknown@example.com',
     packages=[
         'sonic_platform',
     ],
-    package_dir={
-        'sonic_platform': '../../../../device/{}/{}/sonic_platform'.format(DEVICE_NAME, HW_SKU)},
+    #package_dir={
+    #    'sonic_platform': '../../../../device/{}/{}/sonic_platform'.format(DEVICE_NAME, HW_SKU)},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
